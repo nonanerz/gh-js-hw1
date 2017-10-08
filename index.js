4d651a1874cac69f7dec47d6c5776255ff132d25
Bulava Eduard <bulavaeduard@gmail.com>
2017-10-08 14:12:23 +0300

    fix

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var first = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
     second = [-1,-8,-2];
-    third = [1,7,3];
-    fourth = [1,undefined,3,5,-3];
-    fifth = [1,NaN,3,5,-3];
+    third = [undefined, 1,7,3];
+    fourth = [undefined, 1,undefined,3,5,-3];
+    fifth = [undefined, 1,NaN,3,5,-3];
 
 var findMinMaxSum = function (array) {
 
     var sum = null;
-        min = array[0];
-        max = array[0];
+        min = null;
+        max = null;
 
     for(var key in array) {
 
         if (!isNaN(array[key]) && typeof (array[key]) !== 'string') {
             sum += array[key];
-            min = array[key] < min ? array[key] : min;
-            max = array[key] > max ? array[key] : max;
+            min = (array[key] < min || min === null) ? array[key] : min;
+            max = (array[key] > max || max === null) ? array[key] : max;
         }
     }
 
